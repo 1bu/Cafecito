0162568905e57c744a4d3ccb075fc473ee7fe1b2
Iván Carpio <67491616+1bu@users.noreply.github.com>
2026-01-31 15:53:05 -0500

    railway config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,4 +2,4 @@ const isDevelopment = window.location.hostname === 'localhost';
 
 export const API_URL = isDevelopment
   ? 'http://localhost:3001' 
-  : 'http://cafecito-production-eab0.up.railway.app'
+  : 'https://cafecito-production-eab0.up.railway.app'
